3dd732325daf52d19cdc345ae688e654740769d0
Felipe Liberman Fuchs <felipe@almosthackers.net>
2021-11-30 14:44:41 -0300

    Rename services to make them CamelCase

--- js/index.js
+++ js/index.js
@@ -1,37 +1,34 @@
-window.addEventListener("DOMContentLoaded",()=> {
+window.addEventListener("DOMContentLoaded", () => {
     createData();
 })
 
-function createData(){
-    if (!sessionStorage.getItem("agendamentos")){
-        sessionStorage.setItem("agendamentos",JSON.stringify([
-            {
-                'name':'José',
-                'services': [0,1],
+function createData() {
+    if (!sessionStorage.getItem("agendamentos")) {
+        sessionStorage.setItem("agendamentos", JSON.stringify([{
+                'name': 'José',
+                'services': [0, 1],
                 'time': '11:30'
             },
             {
-                'name':'Maria',
+                'name': 'Maria',
                 'services': [2],
                 'time': '14:00'
             },
             {
-                'name':'Juliana',
-                'services': [0,3,4,5],
+                'name': 'Juliana',
+                'services': [0, 3, 4, 5],
                 'time': '15:30'
             },
         ]))
     }
-    if (!sessionStorage.getItem("servicos")){
-        sessionStorage.setItem("servicos",JSON.stringify(
-            {
-                0:'corte cabelo',
-                1:'corte barba',
-                2:'manicure',
-                3:'pedicure',
-                4:'escova',
-                5:'maquiagem'
-            }
-        ))
+    if (!sessionStorage.getItem("servicos")) {
+        sessionStorage.setItem("servicos", JSON.stringify({
+            0: 'Corte de Cabelo',
+            1: 'Corte Barba',
+            2: 'Manicure',
+            3: 'Pedicure',
+            4: 'Escova',
+            5: 'Maquiagem'
+        }))
     }
 }
