9e7228a04946d9b64fdac6d9dfc66ee9a3a4d44a
Felipe Liberman Fuchs <felipe@almosthackers.net>
2021-11-30 14:44:32 -0300

    Finish reorganizing calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -1,4 +1,6 @@
+// Read sessionStorage
 const serviceNames = JSON.parse(sessionStorage.getItem('servicos'))
+
 window.addEventListener("DOMContentLoaded", () => {
     const appointments = JSON.parse(sessionStorage.getItem('agendamentos'));
     const apList = document.querySelector('.atendimentos>ul')
@@ -6,24 +8,21 @@ window.addEventListener("DOMContentLoaded", () => {
         console.log(appointment)
         addAppointment(appointment, apList)
     }
-
-
-
-
 })
 
 function addAppointment({ name, services, time }, parent) {
-    // Let's log some stuff to check the function progress
-    console.log(name, services, time)
-
     const apt = document.createElement('li');
     const nameElement = document.createElement('a')
     const servicesP = document.createElement('p')
     const timeP = document.createElement('p')
 
+    // Let's log some stuff to check the function's invocation
+    console.log(name, services, time)
+
     // Set data
     nameElement.innerHTML = name
     timeP.innerHTML = time
+
     servicesP.innerHTML = services.map((i) => serviceNames[i])
 
     // Create the new element
